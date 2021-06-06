ffc86b7259d85a430832d76be4d9fcb230add325
mtugrulavci <mustafa.tugrulavci@alcumus.com>
2021-06-06 16:09:33 -0400

    add array

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const profileDataArgs = process.argv.slice(2);
+
 const printProfileData = profileDataArr => {
   // This...
   for (let i = 0; i < profileDataArr.length; i += 1) {
@@ -7,7 +9,7 @@ const printProfileData = profileDataArr => {
   console.log('================');
 
   // Is the same as this...
-  profileDataArr.forEach((profileItem) => {
-    console.log(profileItem)
-  });
+  profileDataArr.forEach(profileItem => console.log(profileItem));
 };
+
+printProfileData(profileDataArgs);
